cf0a7ce6dc901315fcc99fb1960fac3d715b2769
dileepkumar <dileep123456@gmail.com>
2024-12-05 00:08:27 +0530

    coment added

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import json
 import time
 import DbUtil
 
+# this is about database connection
 dbconnect = DbUtil.getDbConnection()
 c = dbconnect.cursor()
 c.execute('IF EXISTS(SELECT	* FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = "loan_data")')
